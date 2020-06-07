61ad83c568f615234280abb79de2ca0b8541d090
Dave Cheney <dave@cheney.net>
2020-06-07 21:45:13 +1000

    Add examples

---
+++ example_test.go
@@ -0,0 +1,71 @@
+package json_test
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"strings"
+
+	"github.com/pkg/json"
+)
+
+func ExampleScanner_Next() {
+	input := `{"a": 1,"b": 123.456, "c": [null]}`
+	sc := json.NewScanner(strings.NewReader(input))
+	for {
+		tok := sc.Next()
+		if len(tok) < 1 {
+			break
+		}
+		fmt.Printf("%s\n", tok)
+	}
+	if err := sc.Error(); err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+
+	// Output:
+	// {
+	// "a"
+	// :
+	// 1
+	// ,
+	// "b"
+	// :
+	// 123.456
+	// ,
+	// "c"
+	// :
+	// [
+	// null
+	// ]
+	// }
+
+}
+
+func ExampleDecoder_Token() {
+	input := `{"a": 1,"b": 123.456, "c": [null]}`
+	dec := json.NewDecoder(strings.NewReader(input))
+	for {
+		tok, err := dec.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", tok)
+	}
+
+	// Output:
+	// {
+	// "a"
+	// 1
+	// "b"
+	// 123.456
+	// "c"
+	// [
+	// null
+	// ]
+	// }
+
+}
